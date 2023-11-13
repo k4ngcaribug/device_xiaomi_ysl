b627f37640de480492ca895036908893cbdd0cae
Giovanni Ricca <giovanniricca@proton.me>
2023-11-13 14:45:45 +0000

    ysl: Build the upstreamed biometrics hal
    Change-Id: Iebe1dca707f6754671a7a08f07c9b7564f4f44b2

--- device.mk
+++ device.mk
@@ -163,7 +163,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953-32
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_msm8953-32
 
 # FM
 PRODUCT_PACKAGES += \
