5cc0d91d8c47c0eaf7c0416de164f0138481ea91
AL <168145100+ALprjkt@users.noreply.github.com>
2024-07-01 18:00:10 +0700

    ysl: Spoof FP to walleye

--- qassa_ysl.mk
+++ qassa_ysl.mk
@@ -44,4 +44,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ysl-user 8.1.0 OPM1.171019.011 V10.3.4.0.OEFCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/ysl/ysl:9/PKQ1.181203.001/V12.0.2.0.PEFMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
