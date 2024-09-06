bda603826c844e80f8ef040e9b42b093495af360
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-09-06 14:31:59 +0000

    ysl: Update removepackages
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,34 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Camera2 Maps Drive PrebuiltGmail YouTube Videos
+LOCAL_OVERRIDES_PACKAGES := \
+	AudioFX \
+	Aperture \
+	arcore \
+	AICorePrebuilt \
+	AiWallpapers \
+	AndroidAutoStubPrebuilt \
+	Camera2 \
+        DevicePersonalizationPrebuiltPixel2020 \
+        DuckDuckGo \
+        Flash \
+	GoogleTTS \
+	Gallery2 \
+	Jellyfish \
+	Jelly \
+	Maps \
+	Photos \
+	PixelThemesStub \
+	PixelThemesStub2022_and_newer \
+	PixelWallpapers2023 \
+	PixelLiveWallpaperPrebuilt \
+	SnapCamera \
+	SafetyHubPrebuilt \
+        SimpleGallery \
+	TurboPrebuilt \
+	Velvet \
+	WellbeingPrebuilt \
+	YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
