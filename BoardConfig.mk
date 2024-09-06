b33df7321b5d9dc9c5d71e6814b50aa5cfff5099
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-09-06 11:40:48 +0000

    ysl: Update kernel config
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ysl
-TARGET_KERNEL_CONFIG += xiaomi/ysl.config
+TARGET_KERNEL_CONFIG += ysl_defconfig
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
