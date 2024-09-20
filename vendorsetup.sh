b21f09da3540d87c4277bc204157fc8872bb96d4
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-09-20 07:21:46 +0000

    Revert "ysl: Track Hals from ours github"
    This reverts commit 2b70dd1909164555a6fe958ae2da36f9ce346b00.

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf hardware/qcom-caf/msm8996
-git clone --depth=1 -b 13 https://github.com/ALprjkt/android_hardware_qcom_display hardware/qcom-caf/msm8996/display
-git clone --depth=1 -b 13 https://github.com/ALprjkt/android_hardware_qcom_media hardware/qcom-caf/msm8996/media
-git clone --depth=1 -b 13 https://github.com/ALprjkt/android_hardware_qcom_audio hardware/qcom-caf/msm8996/audio
