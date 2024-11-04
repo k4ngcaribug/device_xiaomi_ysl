63b375f13aa986cb2e278ad1b1d1768d684f227f
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2024-11-04 16:25:44 +0000

    ysl: Track fwb from ours gith
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,5 @@ rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
 git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_display -b lineage-17.1-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone --depth=1 https://github.com/LineageOS/android_hardware_qcom_media -b lineage-17.1-caf-msm8996 hardware/qcom-caf/msm8996/media
+rm -rf frameworks/base
+git clone --deth=1 https://github.com/k4ngcaribug/frameworks_base frameworks/base
